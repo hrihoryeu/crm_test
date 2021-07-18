7f600be506bf719d570697c9b4aa690db1302db2
hrihoryeu <egoragrig@gmail.com>
2021-07-18 14:28:23 +0300

    created new models of Employee and Companies

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django_countries import fields
 
-# Create your models here.
+
+class Company(models.Model):
+    title = models.CharField(max_length=30)
+    location = fields.CountryField()
+    number_of_offices = models.IntegerField()
+    number_of_employees = models.IntegerField()
+    establishment = models.DateField(auto_created=True)
+    cooperation = models.ManyToManyField('self', blank=True)
+
+
+class Employee(models.Model):
+    birth_date = models.DateField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    company = models.ManyToManyField(Company)
+    position = models.JSONField()
